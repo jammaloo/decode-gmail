150eb2306ead2392591de8255c3789fff0eef184
James Freeman <jfreeman@activecampaign.com>
2016-06-11 15:55:24 -0400

    Basic reformatting
    Fixed up some comments, formatting and chnages spaces to tabs

--- content.js
+++ content.js
@@ -1,18 +1,17 @@
 // Listen for messages
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
 	if (msg.greeting === 'GetEmailSource') {
-		//we're on the message source page
+		//we're on the message source page, so just get the page content
 		if(window.location.href.indexOf('?ui=2&ik=') != -1) {
 			sendResponse({source: window.document.body.innerText});
 			return;
-		} else {
-
 		}
 		var ik_value = window.document.head.getAttribute('data-inboxsdk-ik-value');
 		var message_id = window.location.hash.split('/').pop();
 		if(!message_id) {
 			return;
 		}
+		//build the URL for the raw message page, then grab the source
 		var raw_url = '//' + window.location.hostname + window.location.pathname + '?ui=2&ik=' + ik_value + '&view=om&th=' + message_id;
 		var xhr = new XMLHttpRequest();
 		xhr.open('GET', raw_url, true);
