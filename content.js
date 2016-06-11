c479be09038fd8b20dad172072fda46b0051fa7f
James Freeman <jfreeman@activecampaign.com>
2016-06-11 17:13:47 -0400

    Made addon firefox compatible

--- content.js
+++ content.js
@@ -6,13 +6,18 @@ chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
 			sendResponse({source: window.document.body.innerText});
 			return;
 		}
-		var ik_value = window.document.head.getAttribute('data-inboxsdk-ik-value');
+		var script = document.createElement('script');
+		var scriptContent = "document.body.setAttribute('data-gmaildecode-ik-value', GLOBALS[9]);";
+		script.appendChild(document.createTextNode(scriptContent));
+		(document.body || document.head || document.documentElement).appendChild(script);
+
+		var ik_value = window.document.body.getAttribute('data-gmaildecode-ik-value');
 		var message_id = window.location.hash.split('/').pop();
 		if(!message_id) {
 			return;
 		}
 		//build the URL for the raw message page, then grab the source
-		var raw_url = '//' + window.location.hostname + window.location.pathname + '?ui=2&ik=' + ik_value + '&view=om&th=' + message_id;
+		var raw_url = window.location.protocol + '//' + window.location.hostname + window.location.pathname + '?ui=2&ik=' + ik_value + '&view=om&th=' + message_id;
 		var xhr = new XMLHttpRequest();
 		xhr.open('GET', raw_url, true);
 		xhr.onload = function() {
